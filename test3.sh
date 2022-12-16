647f0fc15b8f80661318d06c2e96ee39ab886e6c
Taylor Monacelli <taylormonacelli@gmail.com>
2022-12-16 09:48:48 -0500

    real men dont make git commit comments

--- test3.sh
+++ test3.sh
@@ -8,15 +8,6 @@ set -e
 set -x
 
 myscript=$(mktemp /tmp/test3-scratch.XXXX)
-chmod +x $myscript
-script --flush --command $myscript </dev/null
-rm -f $myscript
-passphrase=$(grep Passphrase typescript | sed -e 's#Passphrase: *##' | tr -dc '[[:print:]]')
-echo $passphrase
-echo -n $passphrase | xxd -c 1
-
-gpg --version
-
 cat >$myscript <<EOF
 #!/bin/bash
 
@@ -25,6 +16,14 @@ gopass config core.nocolor true
 gopass config
 gopass --yes setup --create --alias pass --name "Your Name" --email yourname@example.com
 EOF
+chmod +x $myscript
+script --flush --command $myscript </dev/null
+rm -f $myscript
+passphrase=$(grep Passphrase typescript | sed -e 's#Passphrase: *##' | tr -dc '[[:print:]]')
+echo $passphrase
+echo -n $passphrase | xxd -c 1
+
+gpg --version
 
 rm -f /tmp/secret-key-backup.asc
 gpg --export-secret-keys --pinentry-mode=loopback --armor --passphrase="$passphrase" --output=/tmp/secret-key-backup.asc
