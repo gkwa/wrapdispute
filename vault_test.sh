d0286cee43caa309e29d3641a66796dda6975c11
Taylor Monacelli <taylormonacelli@gmail.com>
2022-12-16 23:22:53 -0500

    real men dont make git commit comments

--- vault_test.sh
+++ vault_test.sh
@@ -3,10 +3,13 @@
 # docker run --rm --workdir /tmp -v $(pwd):/tmp -ti taylorm/wrapdispute bash
 # bash -x vault_test.sh
 
+# https://gist.github.com/jan-warchol/bd6340a8b49f0033aec5fbe3e9aa10d5
+
 gpg --batch --passphrase 'mysecret' --quick-gen-key john.doe@gmail.com default default
 gpg --list-keys
 pass init -p aws-vault john.doe@gmail.com
-gpg --list-keys
+
+gpg --batch --edit-key john.doe@gmail.com quit
 
 export AWS_VAULT_BACKEND=pass
 export AWS_VAULT_PASS_PASSWORD_STORE_DIR=/root/.password-store/aws-vault
@@ -29,6 +32,8 @@ aws configure set profile.my_test_profile.region us-west-2
 cat ~/.aws/config
 
 pass ls
+
+echo secret is mysecret
 pass show aws-vault/my_test_profile
 
 aws-vault exec my_test_profile -- aws --region us-west-2 s3 ls s3://mybucket
